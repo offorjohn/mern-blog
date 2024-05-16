187d8dea4d7b20fb3ea59536e38a4048b9766f0b
offor <offorchukwuebuka8@gmail.com>
2024-05-16 22:00:20 +0100

    add preinstalation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,34 @@
-export default function About(){
-
-
+export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About John's Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Hello my name is john and Welcome to my portfolio Blog Thankyou
+             
+            
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Sahand is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
